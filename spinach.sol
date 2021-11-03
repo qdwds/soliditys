3b9e1bc750a2d7f76e0827e40672d94678a34080
yang <qdwds94@gmail.com>
2021-11-03 20:28:57 +0800

    init

---
+++ spinach.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9;
+
+contract Bet {
+    address public owner;   // 管理员
+    bool.isFinshed；    // 游戏结束标志
+    
+    // 玩家结构信息，可以记录玩家及其下下注金额
+}
---
+++ transfer.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * 银行实现
+ *  存钱
+ *  取钱
+ *  查看余额
+ */
+
+ contract SmallBank {
+     // 存钱数据库
+     mapping 
+ }
